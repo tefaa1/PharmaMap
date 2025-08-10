767262da5673de6a43ad88c9a3bd805d135493e2
tefaa1 <mh8369629@gmail.com>
2025-08-10 20:36:52 +0300

    make the initial impl of service user #5

---
+++ AuthService/src/main/java/com/spring/authservice/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.spring.authservice.service;
+
+import com.spring.authservice.dto.request.*;
+import com.spring.authservice.dto.response.AuthenticationResponseDto;
+import com.spring.authservice.dto.response.UserResponseDto;
+import com.spring.authservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public void register(RegisterDto registerDto) {
+
+    }
+
+    @Override
+    public AuthenticationResponseDto login(LoginDto loginDto) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationResponseDto refreshAccessToken(RefreshTokenDto refreshTokenDto) {
+        return null;
+    }
+
+    @Override
+    public void changePassword(ChangePasswordDto changeDto) {
+
+    }
+
+    @Override
+    public void changeTheName(ChangeNameDto changeName) {
+
+    }
+
+    @Override
+    public UserResponseDto getTheCurrentUser() {
+        return null;
+    }
+
+    @Override
+    public void deleteTheCurrentUser() {
+
+    }
+}
