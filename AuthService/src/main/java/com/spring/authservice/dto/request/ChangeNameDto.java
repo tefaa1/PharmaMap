27bbe6ed18a6ed808f68af369bd40a71e61fee7f
tefaa1 <mh8369629@gmail.com>
2025-08-10 15:18:48 +0300

    Create ChangeDtos #3

---
+++ AuthService/src/main/java/com/spring/authservice/dto/request/ChangeNameDto.java
@@ -0,0 +1,15 @@
+package com.spring.authservice.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangeNameDto {
+
+    @NotBlank
+    private String name;
+}
